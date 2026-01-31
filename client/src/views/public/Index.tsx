6874df454e0f09cc9615d248b005524fb6e5aa2a
Maximillian Raphaels <maxralph01@gmail.com>
2026-01-31 09:31:16 +0400

    public header complete, internationalization (language switch) implemented

--- client/src/views/public/Index.tsx
+++ client/src/views/public/Index.tsx
@@ -1,9 +1,11 @@
 import Layout from '@/components/public/Layout.tsx';
+import { useTranslation } from 'react-i18next';
 
 export default function Index() {
+  const { t } = useTranslation();
   return (
     <Layout>
-      hhjhgcv
+      <h1>{t('welcome', { name: 'User' })}</h1>
     </Layout>
   )
 }
