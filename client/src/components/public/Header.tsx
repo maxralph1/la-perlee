832957ddd3a83c6488ce962e18f8a35a1564e395
Maximillian Raphaels <maxralph01@gmail.com>
2026-02-25 18:00:15 +0400

    re-introduced vite @ smart file directory after debugging

--- client/src/components/public/Header.tsx
+++ client/src/components/public/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { route } from '../../routes';
+import { route } from '@/routes';
 import { useTranslation } from 'react-i18next';
 
 export default function Header() {
