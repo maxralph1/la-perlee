19f593e54b9042abb4e18f746ab564d212fd4dea
Maximillian Raphaels <maxralph01@gmail.com>
2026-02-25 18:00:15 +0400

    re-introduced vite @ smart file directory after debugging

--- client/src/views/public/Collection.tsx
+++ client/src/views/public/Collection.tsx
@@ -1,5 +1,5 @@
-import LinkButton from '../../components/LinkButton';
-import Layout from '../../components/public/Layout';
+import LinkButton from '@/components/LinkButton';
+import Layout from '@/components/public/Layout';
 
 export default function Collection() {
   return (
